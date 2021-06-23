2a67e0b9f4a4ebc89494c8707ef8568aa81ef250
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-06-23 23:46:48 +0900

    変数の宣言と代入

--- plactice201/plactice201/Program.cs
+++ plactice201/plactice201/Program.cs
@@ -6,9 +6,19 @@ namespace plactice201
     {
         static void Main(string[] args)
         {
+          // 演算子
             Console.WriteLine("{0} + {1} = {2}", 5, 3, 5 + 3 );
             Console.WriteLine("{0} / {1} = {2}", 4, 2, 4 / 2);
             Console.WriteLine("{0} / {1} = {2} 余り{3}", 5, 2, 5 / 2, 5 % 2);
+
+            // 変数
+            int a = 3;  // int型で整数を代入
+
+            int b = 4, c = 5; // int型の変数を複数宣言 ※複数の代入はできない
+
+
+            Console.WriteLine($"{a}{b}{c}" );
+            
 
         }
     }
