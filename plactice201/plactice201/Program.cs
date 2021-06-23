3cc6cd106d836b36585faa0b83ad0b343ae07169
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-06-23 23:31:54 +0900

    演算子

---
+++ plactice201/plactice201/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace plactice201
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} + {1} = {2}", 5, 3, 5 + 3 );
+            Console.WriteLine("{0} / {1} = {2}", 4, 2, 4 / 2);
+            Console.WriteLine("{0} / {1} = {2} 余り{3}", 5, 2, 5 / 2, 5 % 2);
+
+        }
+    }
+}
