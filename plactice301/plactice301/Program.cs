3cccd10cb2b460503a271ef49d61623dc97e72b3
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-06-27 17:20:29 +0900

    else文について

--- plactice301/plactice301/Program.cs
+++ plactice301/plactice301/Program.cs
@@ -6,7 +6,7 @@ namespace plactice301
     {
         static void Main(string[] args)
         {
-            Console.Write("整数値を入力してください");
+            Console.Write("整数値を入力してください: ");
             int a = int.Parse(Console.ReadLine());  // int.parse　で標準入力を数値型に変換
             Console.WriteLine("a= " + a);    // "a=" + 整数型でも結合できる
 
@@ -15,6 +15,11 @@ namespace plactice301
             {
                 // true 真の場合の処理
                 Console.WriteLine($"a {a} は正の数です");
+            }
+            else
+            {
+                // false　偽の場合の処理
+                Console.WriteLine("a は正の数ではありません");
             }
 
         }
