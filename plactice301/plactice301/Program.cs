87ea1f9bf588a6944827ac79b4d4f3afb08d8394
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-06-27 17:18:32 +0900

    if文の基本構文

--- plactice301/plactice301/Program.cs
+++ plactice301/plactice301/Program.cs
@@ -6,7 +6,17 @@ namespace plactice301
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("整数値を入力してください");
+            int a = int.Parse(Console.ReadLine());  // int.parse　で標準入力を数値型に変換
+            Console.WriteLine("a= " + a);    // "a=" + 整数型でも結合できる
+
+            // 条件分岐 if文
+            if (a > 0)
+            {
+                // true 真の場合の処理
+                Console.WriteLine($"a {a} は正の数です");
+            }
+
         }
     }
 }
