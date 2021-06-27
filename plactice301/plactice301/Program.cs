5c558713065fe46ec0dc66a599e3cda22f74ade6
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-06-27 17:34:29 +0900

    if文のネストと論理積

--- plactice301/plactice301/Program.cs
+++ plactice301/plactice301/Program.cs
@@ -7,7 +7,8 @@ namespace plactice301
         static void Main(string[] args)
         {
             Console.Write("整数値を入力してください: ");
-            int a = int.Parse(Console.ReadLine());  // int.parse　で標準入力を数値型に変換
+            //int a = int.Parse(Console.ReadLine());  // int.parse　で標準入力を数値型に変換
+            int a = 3;
             Console.WriteLine("a= " + a);    // "a=" + 整数型でも結合できる
 
             // 条件分岐 if文
@@ -18,7 +19,7 @@ namespace plactice301
             }
             else if (a == -5 || a == -3)
             {
-                Console.WriteLine("a は5もしくは3です");
+                Console.WriteLine("a は-5もしくは -3です");
             }
             else
             {
@@ -26,6 +27,27 @@ namespace plactice301
                 Console.WriteLine("a は正の数ではありません");
             }
 
+
+            // if文の論理式 と if文のネスト
+            Console.Write("サイコロの目を入力: 1 ~ 6: ");
+
+            int dice = int.Parse(Console.ReadLine());
+
+            if (1 <= dice && 6 >= dice)
+            {
+                if (dice == 2 || dice == 4 || dice == 6)
+                {
+                    Console.WriteLine("サイコロの目は’丁'です");
+                }
+                else if (dice == 2 || dice == 4 || dice == 6)
+                {
+                    Console.WriteLine("サイコロの目は’半'です");
+                }
+            }
+            else
+            {
+                Console.WriteLine("1〜6で入力してください");
+            }
         }
     }
 }
