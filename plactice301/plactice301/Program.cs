b3d1a3a0ef22394990d09630091ddc8dde811a8e
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-06-30 00:25:03 +0900

    cocoa

--- plactice301/plactice301/Program.cs
+++ plactice301/plactice301/Program.cs
@@ -29,9 +29,9 @@ namespace plactice301
 
 
             // if文の論理式 と if文のネスト
-            //Console.Write("サイコロの目を入力: 1 ~ 6: ");
-
-            int dice = int.Parse(Console.ReadLine());
+            Console.Write("サイコロの目を入力: 1 ~ 6: ");
+            //int dice = int.Parse(Console.ReadLine());
+            int dice = 4;
 
             if (1 <= dice && 6 >= dice)
             {
@@ -52,7 +52,8 @@ namespace plactice301
 
             // switch文について
             Console.Write("1から3を入力してください");
-            int num = int.Parse(Console.ReadLine());
+            //int num = int.Parse(Console.ReadLine());
+            int num = 1;
 
             switch(num){
                 case 1:
@@ -77,7 +78,69 @@ namespace plactice301
                     break;
 
             }
-            
+
+            // 例題1 double型の変数tempを宣言して標準入力で数値を入力し条件式にそった出力
+
+            Console.WriteLine("数字を入力してください");
+            double temp = 1.2;
+
+            if (temp == 0.0)
+            {
+                Console.WriteLine("ゼロォ");
+            }
+            else if (temp >= 0)
+            {
+                Console.WriteLine("正の数");
+            }
+            else if (temp <= 0)
+            {
+                Console.WriteLine("負の数 ");
+            }
+            else
+            {
+                Console.WriteLine("数字を入力してください");
+
+            }
+
+            // 例題2　文字列型 s1, s2を標準入力して, s1がHello s2がworldの際の条件式
+
+            Console.Write("s1の文字入力をしてください");
+            string s1 = Console.ReadLine();
+
+            Console.Write("s2の文字入力をしてください");
+            string s2 = Console.ReadLine();
+
+            if (s1 == "hello" && s2 == "world")
+            {
+                Console.WriteLine($"{s1 + s2} please!!");
+            }
+            else
+            {
+                Console.WriteLine("s1にhelloをs2にworldを入力してちょ");
+            }
+
+            // 例題3　信号機の処理を赤青黄で処理しなさい
+            Console.Write("信号の色は");
+            string color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "赤":
+                    Console.Write("赤は止まれ！");
+                    break;
+
+                case "青":
+                    Console.Write("青は進め");
+                    break;
+
+                case "黄色":
+                    Console.Write("黄色まだまだ赤勝負");
+                    break;
+                default:
+                    Console.Write($"{color}は信号の色にはありません");
+                    break;
+            }
+                
 
         }
     }
