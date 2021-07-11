2e3ffe0c49d50c05f207b413432fdad3ec375d2c
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-07-11 17:27:20 +0900

    commit

--- branch/branch/Program.cs
+++ branch/branch/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections;
 
 namespace branch
 {
@@ -106,7 +106,72 @@ namespace branch
             DateTime dt = new DateTime(p.Next(1990, 2021), p.Next(1, 12), p.Next(1, 29));
             Console.WriteLine(dt);
 
-            
+            var week = new Hashtable(); //ハッシュテーブルの作成
+            week["0"] = "日曜日";
+            week.Add("1", "月曜日");
+            week.Add("2", "火曜日");
+            week.Add("3", "水曜日");
+            week.Add("4", "木曜日");
+            week.Add("5", "金曜日");
+            week.Add("6", "土曜日");
+
+            var times = new Hashtable();
+            times["0"] = "午前中";
+            times["1"] = "午後";
+            times["2"] = "夜間";
+
+            int w = p.Next(0, 6);
+            int t = p.Next(0, 2);
+
+            Console.Write($"曜日{w}:時間帯{t}:");
+            if (w == 0 )
+            {
+                Console.WriteLine("休診です");
+            }
+            else if (w == 6 && t == 0)
+            {
+                Console.WriteLine("休診です");
+            }
+            else if ( (w == 2 || w == 5) && t == 0)
+            {
+                Console.WriteLine("休診です");
+            }
+            else if (w == 3 && t == 2)
+            {
+                Console.WriteLine("休診です");
+            }
+            else
+            {
+                Console.WriteLine("バリバリ診療中");
+            }
+
+            // 問8 switch文を使用した寿司占い
+
+            Random s = new Random();
+
+            Console.WriteLine("へいらっしゃい！何を握ります？");
+            int sushi = s.Next(1, 5);
+
+            switch (sushi)
+            {
+                case 1:
+                    Console.WriteLine("へいお待ち！！江戸から続くハンバーグ寿司だよ");
+                    break;
+
+                case 2:
+                    Console.WriteLine("へいお待ち！！代々受け継がれたエビフライ寿司だよ");
+                    break;
+
+                case 3:
+                    Console.WriteLine("へいお待ち！！将軍様も御用達のサーモンアボガド握りだよ");
+                    break;
+
+                default:
+                    Console.WriteLine("ガリしかないよ");
+                    break;
+            }
+
+
         }   
 
     }
---
+++ out_ref/out_ref/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace out_ref
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // out修飾子について
+            Calc(2, 3, out int ans); // outパラメータに変数を渡すと同時に変数宣言
+                                     // バージョンに引数内の宣言方法が変わる
+            Console.WriteLine(ans);
+
+            // ------------------------------
+
+
+            // ref 修飾子について
+            int num = 0;   // ref修飾子は必ずメソッド呼出元で値の割り当てが必要
+
+            Add(ref num);
+            Console.WriteLine($"1回目のAdd: {num}");
+            Add(ref num);
+            Console.WriteLine($"2回目のAdd: {num}");
+
+        }
+
+        // out修飾子でメソッドの引数を参照渡し
+        static void Calc(int a, int b, out int ans)
+        {
+            ans = a + b;　// out修飾子の引数は必ず呼出元のメソッド内で値の割り当てが必要になる。
+                         // コメントアウトするとエラーになる
+        }
+
+
+        // ref修飾子でメソッドの引数を参照渡し
+        static void Add(ref int num)
+        {
+            //num = num + 5;  //outと違いコメントアウトでもエラーにならない。
+
+        }
+
+    }
+}
