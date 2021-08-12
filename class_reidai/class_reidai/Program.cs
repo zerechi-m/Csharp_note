0e8cf5979984e5f4e2b64f4bad551ac281bc7a13
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-08-12 17:25:13 +0900

    class_instance

--- class_reidai/class_reidai/Program.cs
+++ class_reidai/class_reidai/Program.cs
@@ -6,7 +6,30 @@ namespace class_reidai
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Vecter2D v1 = new Vecter2D();
+            v1.x = 1.0;
+            v1.y = 1.0;
+            Console.WriteLine($"初期値の表示：v1 = ({v1.x},{v1.y})");
+
+            Vecter2D v2 = new Vecter2D();
+            v2.x = 1.0;
+            v2.y = -1.0;
+            Console.WriteLine($"初期値の表示：v2 = ({v2.x},{v2.y})");
+
+            // -- Vecter2D の Addメソッドを使用
+            v1.Add(v2);
+            Console.WriteLine($"v1 = ({v1.x},{v1.y})");
+
+            // -- Vecter2D の Subメソッドを使用
+            v1.Sub(v2);
+            Console.WriteLine($"v1 = ({v1.x},{v1.y})");
+
+            // -- Vecter2D の Mulメソッドを使用
+            v1.Mul(2.0);
+            Console.WriteLine($"v1 = ({v1.x},{v1.y}) ");
+
+            // -- Vecter2D の DotProductメソッドを使用
+            Console.WriteLine($"v1とv2の内積 = {v1.DotProduct(v2)}");
         }
     }
 }
---
+++ class_reidai/class_reidai/Vecter2D.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace class_reidai
+{
+    public class Vecter2D
+    {
+        //クラスのフィールド変数
+        public double x = 0.0;
+        public double y = 0.0;
+
+        // 他のベクトルの値を足すメソッド
+        public void Add(Vecter2D v)  //実引数は v2 とインスタンスを渡しているので 型はクラス名になる
+        {
+            this.x += v.x;
+            this.y += v.y;
+        }
+
+        // 他のベクトルの値をひくメソッド
+        public void Sub(Vecter2D v)
+        {
+            this.x -= v.x;
+            this.y -= v.y;
+        }
+
+        // ベクトルのスカラー倍を行うメソッド
+        public void Mul(double k)
+        {
+            this.x *= k;
+            this.y *= k;
+        }
+
+        // 他のベクトルの内積を計算するメソッド
+        public double DotProduct( Vecter2D v)
+        {
+            return x * v.x + y * v.y;
+            
+        }
+    }
+}
