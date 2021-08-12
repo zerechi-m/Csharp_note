83ca1eee3f2504d6e76613c101f3bd764db944b3
zerechi-m <tinberutinberu@yahoo.co.jp>
2021-08-12 17:45:16 +0900

    proparty

---
+++ access_pla/access_pla/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace access_pla
+{
+    public class Person
+    {
+        //名前 (フィールド)
+        private string name = "";
+        //年齢 (フィールド)
+        private int age = 0;
+
+        //プロフィール情報の設定
+        public void SetNameAndAge(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        //情報の表示
+        public void ShowNameandAge()
+        {
+            Console.WriteLine($"名前：{name}, 年齢：{age}");
+        }
+
+        //情報の設定 (プロパティ)
+        public string Name
+        {
+            set { name = value; } // フィールドの　name に対して値の設定 *value は外から与えられた値
+            get { return name; }　// 値の取得
+        }
+
+        public int Age
+        {
+            set { age = value; }　// フィールドの　age に対して値の設定
+            get { return age; }   // 値の取得
+        }
+    }
+}
---
+++ access_pla/access_pla/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace access_pla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1, p2;
+            p1 = new Person();
+            p2 = new Person();
+
+            p1.Name = "山田太郎";
+            p1.Age = 19;
+
+            p2.SetNameAndAge("山田花子", 23);　// Personクラス内のメソッドにて値の設定なのでprivateでも変更設定可能
+            p1.ShowNameandAge();
+            p2.ShowNameandAge();　// 山田花子, 23歳が出力される
+
+            //Personクラスのフィールドの値（name, age)を呼び出す方法
+
+            Console.WriteLine($"名前: {p1.Name} 年齢: {p1.Age}"); //p1.nameとすることでプロパティ内の get が呼び出される
+           
+        }
+    }
+}
